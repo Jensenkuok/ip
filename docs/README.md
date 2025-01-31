fe821979b32503d55b14e4799023a59e66f1e6e7
Jensenkuok <jensenkuok2002@gmail.com>
2025-01-31 12:50:46 +0800

    Add Increment: Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Jamal User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Jamal.java
@@ -0,0 +1,16 @@
+public class Jamal {
+    public static void main(String[] args) {
+        String logo = "  OooOoo                         o\n"
+                + "      O                         O \n"
+                + "      o                         o \n"
+                + "      O                         O \n"
+                + "      o  .oOoO' `oOOoOO. .oOoO' o \n"
+                + "      O  O   o   O  o  o O   o  O \n"
+                + "O     o  o   O   o  O  O o   O  o \n"
+                + "`OooOO'  `OoO'o  O  o  o `OoO'o Oo\n";
+        System.out.println("Hello! I'm Jamal\n" + logo);
+        System.out.println("What can i do for you?\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
